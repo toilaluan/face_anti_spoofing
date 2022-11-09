9982eb05d6c82b00f9c88f3f9088742ae3ab75b2
Tran Thanh Luan <92072154+toilaluan@users.noreply.github.com>
2022-11-09 19:42:53 +0700

    Update train.py

--- train.py
+++ train.py
@@ -37,31 +37,25 @@ def seed_everything(seed: int):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
 seed_everything(2301)
+
+
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter("runs/eff_b7_no_aug")
 def validate(model, val_loader, device):
     cpu_device = torch.device('cuda')
-    # model.to(cpu_device)
     model.eval()
     preds = []
     labels = []
     prob_preds = []
     for label, image, _ in tqdm(val_loader):
         image = image[:,:,0,:,:]
-        # print(label)
         label = label.to(cpu_device)
         image = image.to(cpu_device)
-        
-        # image_lbp = image_lbp.to(device)
         outputs = model(image)
         prediction = outputs > 0.5
-        # print(prediction)
         preds.extend(prediction.tolist())
         prob_preds.extend(outputs.tolist())
-        # print(label)
         labels.append(label.item())
-        # print(preds, labels)
-    # print(prob_preds, preds)
     preds = np.array(preds).astype(np.int32)
     labels = np.array(labels)
     # model.to(device)
@@ -96,13 +90,14 @@ val_dataset = FLDDataset("dataset/new_data/train", img_size=img_size, transform=
 train_size = int(1 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, _ = torch.utils.data.random_split(dataset, [train_size, test_size])
-_, test_dataset = torch.utils.data.random_split(val_dataset, [train_size, test_size])
+_, test_dataset = torch.utils.data.random_split(val_dataset, [train_size, test_size])\
 train_loader = DataLoader(train_dataset, batch_size = 2, shuffle=True)
 val_loader = DataLoader(test_dataset, batch_size = 1)
+
+
 epochs = 100
 device = torch.device("cuda")
 model = EfficientNet()
-model.load_state_dict(torch.load("/home/aimenext/luantt/face_liveness_detecion/weights/eff_b7_no_aug_5.pth")['model_state_dict'])
 model.to(device)
 criterion = nn.BCELoss()
 optimizer = optim.AdamW(model.parameters(), lr = 5e-4)
@@ -112,54 +107,46 @@ a_loss = nn.MSELoss()
 min_eer = 0.03
 accum_iter = 4
 t = 0
+
 for epoch in range(epochs):
-    
     model.train()
     epoch_loss = 0.
-    # acc, eer = validate(model, val_loader, device)
-    # model.zero_grad()
     for i, (label, images, ft_images) in enumerate(train_loader):
         t = t % images.shape[2]
         image = images[:,:,t,:,:]
-        # ft_image = ft_images[:,t,:]
-        # ft_image = ft_image.to(device)
         label = label.to(device)
         image = image.to(device)
         outputs = model(image)
         label = label.unsqueeze(1)
-        # loss = criterion(outputs, label)
         loss, ce_loss = sigmoid_focal_loss(outputs, label, reduction='mean', alpha=-1)
-        # # print(ft_image.shape)
-        # loss_ft = a_loss(features, ft_image)
-        # loss = 0.99*loss_cls + 0.05*loss_ft
         loss = loss / accum_iter
         loss.backward()
         if ((i + 1) % accum_iter == 0) or (i + 1 == len(train_loader)):
             optimizer.step()
             optimizer.zero_grad()
         epoch_loss += loss.item()
-        # print(ce_loss.mean().item())
         print("Epoch {}, Lr [{}], Step [{}/{}], Loss [{:.2f}], CE_loss [{:2f}]".format(epoch, scheduler.get_last_lr(), i, len(train_loader), loss.item()*accum_iter, ce_loss.mean().item()))
     t += 1
     scheduler.step()
     writer.add_scalar("Epoch_loss", epoch_loss*accum_iter / len(train_loader), epoch)        
-    # acc, eer_score = validate(model, val_loader, device)
-    # print("Epoch {}, acc [{:.2f}], eer [{:.2f}]".format(epoch, acc, eer_score))
-    # writer.add_scalar("eer", eer_score, epoch)
-    # writer.add_scalar("acc", acc, epoch)
-    path_save_ckpt = 'weights/eff_b7_no_aug_{}.pth'.format(epoch)
-    # if eer_score < min_eer:
-    #     min_eer = eer_score
-    #     torch.save({
-    #         # 'epoch' : epoch,
-    #         'model_state_dict' : model.state_dict(),
-    #         # 'optimizer_state_dict': optimizer.state_dict(),
-    #         # 'loss' : loss.item()
-    #     }, path_save_ckpt)
+    acc, eer_score = validate(model, val_loader, device)
+    print("Epoch {}, acc [{:.2f}], eer [{:.2f}]".format(epoch, acc, eer_score))
+    writer.add_scalar("eer", eer_score, epoch)
+    writer.add_scalar("acc", acc, epoch)
+    path_save_ckpt = 'weights/model_name_{}.pth'.format(epoch)
+    if eer_score < min_eer:
+        min_eer = eer_score
+        torch.save({
+            'epoch' : epoch,
+            'model_state_dict' : model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss' : loss.item()
+        }, path_save_ckpt)
     if epoch % 10 == 0:
         torch.save({
-            # 'epoch' : epoch,
+            'epoch' : epoch,
             'model_state_dict' : model.state_dict(),
-            # 'optimizer_state_dict': optimizer.state_dict(),
-            # 'loss' : loss.item()
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss' : loss.item()
         }, path_save_ckpt)
+    
